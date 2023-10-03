51d43d9fea4df1218ae800cf6882fee15193cccc
Yash Kumar <yashhkumarrrr@outlook.com>
2023-10-03 22:41:51 +0530

    QR-Code-Generator

--- src/components/home.js
+++ src/components/home.js
@@ -1,45 +1,3 @@
-// import './home.css';
-// import { useState } from 'react';
-// import QRCode from 'react-qr-code';
-
-// function Home() {
-
-//     const [value, setValue] = useState();
-//     const [result, setResult] = useState(false);
-
-//     const handleSubmit = (event) => {
-//         setResult(true);
-//         event.preventDefault();
-//     }
-
-//     return (
-//         <div className='body'>
-//             <form onSubmit={handleSubmit}>
-//                 <div>
-//                     <input
-//                         type="text"
-//                         onChange={(e) => setValue(e.target.value)}
-//                     />
-//                 </div>
-
-//                 <button>Submit</button>
-
-//                 <div>
-//                     {result && (
-//                         <div>
-//                             <QRCode
-//                                 value={value}
-//                             />
-//                         </div>
-//                     )}
-//                 </div>
-//             </form>
-//         </div>
-//     );
-// }
-
-// export default Home;
-
 import './home.css';
 import { useState } from 'react';
 import QRCode from 'react-qr-code';
@@ -69,7 +27,7 @@ function Home() {
 
                 <div>
                     {qrCode && (
-                        <div>
+                        <div className='helo'>
                             <QRCode value={qrCode} />
                         </div>
                     )}
